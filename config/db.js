2d141006e470dd3130bbe1ad9b97d737c0cbb664
Leonardo <leonardo-assuncao1@hotmail.com>
2019-04-18 13:43:08 -0300

    make it be

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://leonardogomes_19:X1WEcEXq@cluster0-1dzcg.mongodb.net/test?retryWrites=true'}
+    module.exports = {mongoURI: 'mongodb+srv://leonardogomes_19:X1WEcEXq@cluster0-1dzcg.mongodb.net/blogapp-prod?retryWrites=true'}
 }else{
     module.exports ={mongoURI: 'mongodb://localhost/blogapp'}
 }
